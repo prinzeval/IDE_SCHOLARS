e1fb4c121e8af5e78020869c60932867a4bc49d6
prinzeval <valentineallpowers@gmail.com>
2025-01-22 04:02:39 +0200

    contact left

--- src/components/Divide/divide.jsx
+++ src/components/Divide/divide.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React from 'react';
 import './divide.css';
-import divide_logo from '../../assets/PNG/Logo Primary Teal.png'
+import divide_logo1 from '../../assets/PNG/Logo Primary Teal.png';
+import divide_logo2 from '../../assets/PNG/Logo Primary Black.png';
+import divide_logo3 from '../../assets/PNG/Logo Primary Black.png';
 
 const Divide = () => {
   return (
-      <div className="divide-content">
-        <img src={divide_logo} alt= "Divide-Logo"></img>
-      </div>
+    <div className="divide-content">
+      <img src={divide_logo1} alt="Divide Logo Teal" className="div-logo logo1" />
+      <img src={divide_logo2} alt="Divide Logo Black" className="div-logo logo2" />
+      
+    </div>
   );
-}
+};
 
 export default Divide;
--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,10 +1,43 @@
-import react from 'react';
+import React from 'react';
 import './footer.css';
+import footerlogo from '../../assets/PNG/Logo Secondary White.png'
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>&copy; 2025 Study Abroad Consultants. All rights reserved.</p>
+      <div className="footer-logo">
+        <img src={footerlogo} alt="Footer Logo" />
+      </div>
+      <div className="footer-sections">
+        <div className="footer-section">
+          <h4>Regions</h4>
+          <ul>
+            <li>Africa</li>
+            <li>Asia</li>
+            <li>Australia/Oceania</li>
+            <li>Europe</li>
+            <li>North America</li>
+          </ul>
+        </div>
+        <div className="footer-section">
+          <h4>ICEF Monitor</h4>
+          <ul>
+            <li>About Us</li>
+            <li>Advertising</li>
+            <li>Sponsored Articles</li>
+            <li>Contact Us</li>
+          </ul>
+        </div>
+      </div>
+      <div className="footer-social">
+        <h4>Follow Us</h4>
+        <div className="social-icons">
+          <a href="#"><i className="fab fa-twitter"></i></a>
+          <a href="#"><i className="fab fa-facebook-f"></i></a>
+          <a href="#"><i className="fab fa-linkedin-in"></i></a>
+          <a href="#"><i className="fab fa-youtube"></i></a>
+        </div>
+      </div>
     </footer>
   );
 };
