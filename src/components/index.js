e607869be0593a68a165e498b6c773630553f387
prinzeval <valentineallpowers@gmail.com>
2025-01-22 19:04:42 +0200

    navbar correction

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 export {default as About} from  './About/about'
 export {default as Contact} from  './Contact/contact'
 export {default as Footer} from  './Footer/footer'
-export {default as Navbar} from  './Navbar/Navbar'
+export {default as Navbar} from  './Navbar/navbar'
 export {default as Divide} from  './Divide/divide'
 export {default as Hero} from  './Hero/hero'
 
